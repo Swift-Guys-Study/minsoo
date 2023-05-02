b2a35f1b617e58b91fad637e4153231f2d8aedb6
alstn38 <alstn38@naver.com>
2023-05-02 21:49:15 +0900

    [Feat] smoking_zone 작업 시작

---
+++ smoking_zone/smoke_zone/smoke_zone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  smoke_zone
+//
+//  Created by 강민수 on 2023/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
